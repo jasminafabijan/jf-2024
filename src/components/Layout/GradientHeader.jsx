acfcedb5ea0b780d86f0d31724932cbfcab36b76
Jasmina Fabijan <jf@jasminafabijan.com>
2026-02-06 16:15:08 +0100

    Fixed navigation

--- src/components/Layout/GradientHeader.jsx
+++ src/components/Layout/GradientHeader.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavbarComponent from '../Navbar/Navbar';
+import './GradientHeader.scss';
 
 function GradientHeader({ children }) {
     return (
         <div className="gradient-background">
             <NavbarComponent />
-            {children}
+            <div className="gradient-background__content">
+                {children}
+            </div>
         </div>
     );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './Navbar.scss';
 import { Link, useLocation } from 'react-router-dom';
@@ -6,9 +6,17 @@ import { Link, useLocation } from 'react-router-dom';
 function NavbarComponent() {
     const location = useLocation();
     const [isOpen, setIsOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => setIsScrolled(window.scrollY > 0);
+        onScroll();
+        window.addEventListener('scroll', onScroll, { passive: true });
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
 
     return (
-        <Navbar className="pt-3">
+        <Navbar className={`pt-3 ${isScrolled ? 'navbar--scrolled' : ''}`}>
             <Container>
                 <Navbar.Brand as={Link} to="/">
                     <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Logo - Jasmina Fabijan" />
